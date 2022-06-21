ced42b116967a5f7d8933dd1db71d14b72d502dd
Wendy <misswendymoira@gmail.com>
2022-06-21 14:41:31 -0700

    added gallery to artistry section
    not perfected yet but want to try using this style of gallery for the work section

--- js/index.js
+++ js/index.js
@@ -44,3 +44,37 @@ function workClose() {
     document.getElementById("workOverlay").style.display = "none"; 
   }
   
+function artistryOn() {
+document.getElementById("galleryOverlay").style.display = "block";
+}
+
+function galleryClose() {
+    document.getElementById("galleryOverlay").style.display = "none"; 
+    }
+
+// let slideIndex = 1;
+// showSlides(slideIndex);
+
+// function plusSlides(n) {
+//     showSlides(slideIndex += n);
+// }
+
+// function currentSlide(n) {
+//     showSlides(slideIndex = n);
+// }
+
+// function showSlides(n) {
+//     let i;
+//     let slides = document.getElementsByClassName("galleryPics");
+//     let dots = document.getElementsByClassName("dot");
+//     if (n > slides.length) {slideIndex = 1}    
+//     if (n < 1) {slideIndex = slides.length}
+//     for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";  
+//     }
+//     for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//     }
+//     slides[slideIndex-1].style.display = "block";  
+//     dots[slideIndex-1].className += " active";
+// }
